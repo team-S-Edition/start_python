aa164d8177d1a467d1fa31bf51af221c94d74d0d
hal5562 <halna5562@gmail.com>
2025-04-18 00:58:40 +0900

    Add files via upload

---
+++ ショートカット.py
@@ -0,0 +1,50 @@
+import keyboard
+import pyautogui
+import subprocess
+while 1:
+    while 1:
+        if keyboard.is_pressed('q') & keyboard.is_pressed("t"):
+            pyautogui.hotkey('ctrlleft','shiftleft','esc')
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("a"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Arduino IDE\Arduino IDE.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("p"):
+            subprocess.run([r"C:\Users\halna\MaixPy IDE\bin\maixpyide.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("l"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\LINE\bin\LineLauncher.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("b"):
+            subprocess.run([r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("s"):
+            subprocess.run([r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"])
+            subprocess.run([r"C:\Users\halna\AppData\Local\LINE\bin\LineLauncher.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        elif keyboard.is_pressed('q') & keyboard.is_pressed("v"):
+            subprocess.run([r"C:\Users\halna\AppData\Local\Programs\Microsoft VS Code\Code.exe"])
+        
+        if keyboard.is_pressed('esc'):
+            break
+    while 1:
+        if keyboard.is_pressed('q') & keyboard.is_pressed('w')&keyboard.is_pressed('e'):
+            break

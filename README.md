b5e3aced13df2edd3bd66e7cfd3a0359d721b842
hal5562 <halna5562@gmail.com>
2025-04-18 00:42:31 +0900

    Update and rename README.md to 禁忌ーデスクトップ複製.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# start_python
-play with python
---
+++ 禁忌ーデスクトップ複製.py
@@ -0,0 +1,6 @@
+import pyautogui  # PyAutoGUIライブラリをインポート
+import pyperclip  # pyperclipライブラリをインポート（クリップボード操作用）
+import time  # timeモジュールをインポート（時間待機用）
+while 1:
+    pyautogui.hotkey('ctrlleft','winleft', 'd')
+    
